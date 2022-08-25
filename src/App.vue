0700540b32e77f8874e35d014916ce7336cebc2a
15227241216 <1469442737@qq.com>
2022-08-25 19:06:45 +0800

    fest: 优化页面布局 :cherries::cherries:

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-  import { reactive, ref, watch, computed } from 'vue'
+  import { ref, watch, computed } from 'vue'
   import { ChangeColor } from './utils/change-color'
   import { debounce } from './utils/utils'
 
@@ -34,46 +34,56 @@
 </script>
 
 <template>
-  <div
-    class="button"
-    :style="{
-      width: width + 'px',
-      height: height + 'px',
-      borderRadius: borderRadius + 'px',
-      fontSize: fontSize + 'px',
-      background,
-      color,
-      ...colorList,
-    }"
-  >
-    <p class="button-text" contenteditable="true">123</p>
+  <div id="box">
+    <div class="button-box">
+      <div
+        class="button"
+        :style="{
+          width: width + 'px',
+          height: height + 'px',
+          borderRadius: borderRadius + 'px',
+          fontSize: fontSize + 'px',
+          background,
+          color,
+          ...colorList,
+        }"
+      >
+        <p class="button-text" contenteditable="true">123</p>
+      </div>
+    </div>
+
+    <ul class="option">
+      <li class="item">
+        <span class="title">背景色</span>
+        <input type="color" v-model="background" />
+      </li>
+      <li class="item">
+        <span class="title">宽度</span>
+        <input type="range" v-model="width" :max="1000" />
+      </li>
+      <li class="item">
+        <span class="title">高度</span>
+        <input type="range" v-model="height" :max="100" />
+      </li>
+      <li class="item">
+        <span class="title">圆角</span>
+        <input type="range" v-model="borderRadius" :max="100" />
+      </li>
+      <li class="item">
+        <span class="title">文字大小</span>
+        <input type="range" v-model="fontSize" :max="30" :min="12" />
+      </li>
+      <li class="item">
+        <span class="title">文字颜色</span>
+        <input type="color" v-model="color" />
+      </li>
+    </ul>
+
+    <div class="result">
+      <f-button round simple>复制 HTML</f-button>
+      <f-button round simple>复制 CSS</f-button>
+    </div>
   </div>
-  <ul class="option">
-    <li class="item">
-      <span class="title">背景色</span>
-      <input type="color" v-model="background" />
-    </li>
-    <li class="item">
-      <span class="title">宽度</span>
-      <input type="range" v-model="width" :max="1000" />
-    </li>
-    <li class="item">
-      <span class="title">高度</span>
-      <input type="range" v-model="height" :max="100" />
-    </li>
-    <li class="item">
-      <span class="title">圆角</span>
-      <input type="range" v-model="borderRadius" :max="100" />
-    </li>
-    <li class="item">
-      <span class="title">文字大小</span>
-      <input type="range" v-model="fontSize" :max="30" :min="12" />
-    </li>
-    <li class="item">
-      <span class="title">文字颜色</span>
-      <input type="color" v-model="color" />
-    </li>
-  </ul>
 </template>
 
 <style lang="scss" scoped>
@@ -82,43 +92,67 @@
     padding: 0;
   }
 
-  .option {
-    .item {
-      list-style: none;
-      color: #a8abb2;
-      line-height: 40px;
-
-      .title {
-        display: inline-block;
-        width: 140px;
+  #box {
+    width: 850px;
+    margin: 0 auto;
+    padding: 30px;
+    box-sizing: border-box;
+    .button-box {
+      height: 200px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .button {
+        height: 35px;
+        color: #fff;
+        font-size: 14px;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 14px;
+        border: none;
+        outline: none;
+        user-select: none;
+        text-decoration: none;
+        transition: 0.3s;
+        line-height: 1;
+        cursor: pointer;
+        padding: 0 25px;
+        overflow: hidden;
+        white-space: nowrap;
+        vertical-align: middle;
+        &:hover {
+          background: var(--button-hover-color) !important;
+        }
+        &:active {
+          background: var(--button-active-color) !important;
+        }
       }
     }
-  }
+    .option {
+      width: 350px;
+      margin: 0 auto;
 
-  .button {
-    height: 35px;
-    color: #fff;
-    font-size: 14px;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 14px;
-    border: none;
-    outline: none;
-    user-select: none;
-    text-decoration: none;
-    transition: 0.3s;
-    line-height: 1;
-    cursor: pointer;
-    padding: 0 25px;
-    overflow: hidden;
-    white-space: nowrap;
-    vertical-align: middle;
-    &:hover {
-      background: var(--button-hover-color) !important;
+      .item {
+        list-style: none;
+        color: #a8abb2;
+        line-height: 40px;
+        display: flex;
+        align-items: center;
+
+        .title {
+          display: inline-block;
+          width: 190px;
+        }
+      }
     }
-    &:active {
-      background: var(--button-active-color) !important;
+    .result {
+      width: 300px;
+      margin: 0 auto;
+      margin-top: 50px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
     }
   }
 </style>
